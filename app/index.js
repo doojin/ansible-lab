996f55621efe659ebcc7c37118f12c20f399d935
doojin <dmitry.papka@gmail.com>
2025-04-16 16:03:15 -0300

    Each app instance should have a randomly generated instance id

--- app/index.js
+++ app/index.js
@@ -3,16 +3,16 @@ const express = require('express');
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8000;
 
 const app = express();
+const instanceId = Math.floor(Math.random() * 1_000_000_000);
 
 app.get('/', (req, res) => {
-    console.log('received request');
+    res.json({ instanceId });
+});
+
+app.get('/ping', (req, res) => {
     res.json({ ok: true });
 });
 
 app.listen(PORT, (error) => {
-    if (error) {
-        console.error(error);
-        return;
-    }
     console.log(`App accepts connections on port ${PORT}`);
 });
